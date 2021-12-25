d516d876370d568cad63369ba225cf8ffee6adca
Raúl <43234547+raulfb@users.noreply.github.com>
2021-12-25 12:38:43 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+let apiKey = "ApiKey";
+ 
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+ 
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+ 
+  pc.oaddIceCandidate = pc.addIceCandidate;
+ 
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+ 
+    console.log(iceCandidate.candidate);
+    const ip = fields[4];
+    if (fields[7] === "srflx") {
+      getDatosUsuario(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+ 
+let getDatosUsuario = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+ 
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+          ---------------------
+	        Ip: ${ip}
+	        Continente: ${json.continent_name}
+          Pais: ${json.country_name}
+          Provincia: ${json.state_prov}
+          Ciudad: ${json.city}
+          Provedor de servicios: ${json.isp}
+          Coordenadas: (${json.latitude}, ${json.longitude})
+          ---------------------
+          `;
+      console.log(output);
+    })
+  );
+};
